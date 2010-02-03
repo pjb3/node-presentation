035a0eb32ade844a075c65ba7743d18c1c01edc0
Paul Barry <mail@paulbarry.com>
2010-02-03 15:56:30 -0500

    Simplified chat server

--- chat_server/chat_server.js
+++ chat_server/chat_server.js
@@ -1,5 +1,52 @@
 tcp = require("tcp")
 puts = require("sys").puts
-Room = require("./room").room
 
-new Room()
+var port = 12345
+
+var connections = []
+var server = tcp.createServer(function(connection) {
+
+  connection.setTimeout(0)
+   
+  connection.send("Nickname: ");
+
+  connection.addListener("receive", function(data) {
+    if(connection.nickname) {
+      server.broadcast(connection.nickname+": "+data.trim()+"\n")
+    } else {
+      connection.nickname = data.trim()
+      connections.push(connection)
+      server.broadcast(connection.nickname+" has joined the room\n")
+    }
+  })
+  
+  connection.addListener("eof", function() {
+    connection.close()
+  })
+  
+  connection.addListener("close", function(has_error) {
+    var i = connections.indexOf(connection)
+    if(i >= 0) {
+      connections.splice(i, 1)
+    }
+    if(connection.nickname) {
+      server.broadcast(connection.nickname+" has left the room\n")
+    } else {
+      puts("connection closed")
+    }
+  })
+  
+})
+
+server.broadcast = function(msg) {
+  connections.forEach(function(c) {
+    if(c.readyState == "open") {
+      c.send(msg)
+    } else{
+      puts("readyState is "+c.readyState)
+    }    
+  })
+}
+
+server.listen(port)
+puts("Chat server listening on port "+port)
